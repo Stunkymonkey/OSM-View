00a73c8bb30f407d56731aa3ff22826b957f8db9
Felix Buehler <account@buehler.rocks>
2017-11-30 15:16:35 +0100

    cleanup Geo Classes stuff

--- src/main/Geo.java
+++ src/main/Geo.java
@@ -1,14 +1,17 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Geo {
-
 	public static void createGrid() {
 		initGrid();
 		createGeoTables();
 	}
 
+	/**
+	 * 
+	 */
 	@SuppressWarnings("unchecked")
 	private static void initGrid() {
 		if (Data.AmountNodes < 10) {
@@ -24,7 +27,13 @@ public class Geo {
 		Data.gridStepSizeY = Data.gridSizeY / Data.gridAmount;
 	}
 
-	public static int[] getGridPosition(double x, double y) {
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	private static int[] getGridPosition(double x, double y) {
 		int x_result, y_result;
 		x_result = (int) ((x - Data.min_x) / Data.gridStepSizeX);
 		y_result = (int) ((y - Data.min_y) / Data.gridStepSizeY);
@@ -34,141 +43,13 @@ public class Geo {
 		if (y_result >= Data.gridAmount) {
 			y_result = Data.gridAmount - 1;
 		}
-		return new int[] { x_result, y_result};
-	}
-//kreigt 2 listen (mit allen kaestchen aus x / y schon davor zusammengefuegt)
-	public static ArrayList<Integer> calcIntersection(int x_index, int y_index) {
-		ArrayList<Integer> intersection = new ArrayList<>();
-		ArrayList<Integer> line = Data.gridX[x_index];
-		ArrayList<Integer> column = Data.gridY[y_index];
-		for (int i : line) {
-			if (column.contains(i)) {
-				intersection.add(i);
-			}
-		}
-		return intersection;
+		return new int[] { x_result, y_result };
 	}
 
-	// mit getGridpos x und y werte holen, damit die arraylisten holen damit die
-	// indize schnittmenge holen
-	public static int getQuarter(double x_click, double y_click, int[] grid) {
-		int quarter = 0;
-		if (grid[0] + Data.gridStepSizeX / 2 >= x_click) {
-			if (grid[1] + Data.gridStepSizeY / 2 >= y_click) {
-				quarter = 1;
-			} else if (grid[1] + Data.gridStepSizeY / 2 <= y_click) {
-				quarter = 3;
-			}
-
-		} else if (grid[0] + Data.gridStepSizeX / 2 <= x_click) {
-			if (grid[1] + Data.gridStepSizeY / 2 >= y_click) {
-				quarter = 2;
-			} else if (grid[1] + Data.gridStepSizeY / 2 <= y_click) {
-				quarter = 4;
-			}
-		}
-		return quarter;
-	}
-	//TODO: getboundsmethode(x,y,r) : typ array[[x1,x2][y1,y2]]
-	//TODO: getPointsinBounds(low,up,dim) : typ linkedliste
-	public static ArrayList<Integer> getPointsAroundClick(double x_click, double y_click) {
-
-		// berechne in welchem viertel von Grid click ist
-		// ist in diesem Kaestchen ein punkt?
-		// Ja:
-		// nehme die 3 umliegenden kaestchen
-		// packe alle punkte aus diesen grids in array -> getclosestpoint
-
-		// Nein:
-		// nehme die 8 umliegenden kaestchen
-		// immer noch nix? weiter nach aussen
-		ArrayList<Integer> pointsAroundClick = new ArrayList<Integer>();
-		int[] grid = getGridPosition(x_click, y_click); // das muss das grid sein in dem der klick ist
-		int x_lower_bound = 0;
-		int x_upper_bound = 0;
-		int y_lower_bound = 0;
-		int y_upper_bound = 0;
-
-		switch (getQuarter(x_click, y_click, grid)) {
-		default:
-			x_lower_bound = -1;
-			x_upper_bound = 1;
-			y_lower_bound = -1;
-			y_upper_bound = 1;
-
-			break;
-		case 1:
-			// oben links
-			x_lower_bound = -1;
-			x_upper_bound = 0;
-			y_lower_bound = 0;
-			y_upper_bound = 1;
-
-			break;
-
-		case 2:
-			// oben rechts
-			x_lower_bound = 0;
-			x_upper_bound = 1;
-			y_lower_bound = 0;
-			y_upper_bound = 1;
-
-			break;
-
-		case 3:
-			// unten links
-			x_lower_bound = -1;
-			x_upper_bound = 0;
-			y_lower_bound = -1;
-			y_upper_bound = 0;
-
-			break;
-
-		case 4:
-			// unten rechts
-			x_lower_bound = 0;
-			x_upper_bound = 1;
-			y_lower_bound = 0;
-			y_upper_bound = 1;
-
-			break;
-		}
-		for (int i = grid[0] + x_lower_bound; i <= grid[0] + x_upper_bound; i++) {
-			for (int j = grid[1] + y_lower_bound; j <= grid[1] + y_upper_bound; j++) {
-				pointsAroundClick.addAll(calcIntersection(i, j));
-			}
-		}
-
-		return pointsAroundClick;
-	}
-
-	public static double getDistance(double x1, double y1, double x2, double y2) {
-		return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-	}
-
-	// kriegt array von umliegenden punkten und berechnet min distanz
-	public static double[] getClosestPoint(double x, double y, ArrayList<Integer> indices) {
-		// fuer x und y int liste mit indizes von buckets
-		// punkt zum vergleichen uebergeben
-		double cp_x = Data.x_dim[0];
-		double cp_y = Data.y_dim[0];
-		double min_distance = Double.MAX_VALUE;
-
-		for (int i = 0; i < indices.size(); i++) {
-			double min_x = Data.x_dim[indices.get(i)];
-			double min_y = Data.y_dim[indices.get(i)];
-			double tmp_distance = getDistance(x, y, min_x, min_y);
-			if (tmp_distance <= min_distance) {
-				min_distance = tmp_distance;
-				cp_x = min_x;
-				cp_y = min_y;
-			}
-		}
-
-		return new double[] { cp_x, cp_y };
-	}
-
-	public static void createGeoTables() {
+	/**
+	 * 
+	 */
+	private static void createGeoTables() {
 		int[] tmp = new int[2];
 		for (int i = 0; i < Data.AmountNodes; i++) {
 			tmp = getGridPosition(Data.x_dim[i], Data.y_dim[i]);
@@ -181,5 +62,135 @@ public class Geo {
 			Data.gridX[tmp[0]].add(i);
 			Data.gridY[tmp[1]].add(i);
 		}
+	}
+
+	/**
+	 * 
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * @return
+	 */
+	private static double getDistance(double x1, double y1, double x2, double y2) {
+		return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	}
+
+	/**
+	 * 
+	 * @param x_index
+	 * @param y_index
+	 * @return
+	 */
+	private static ArrayList<Integer> calcIntersection(ArrayList<Integer> first, ArrayList<Integer> second) {
+		first.retainAll(second);
+		return first;
+	}
+
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public static int getNearestNeighbor(double x, double y) {
+		int[] gridPos = getGridPosition(x, y);
+		ArrayList<Integer> points = getPointsInBound(gridPos[0], gridPos[1], 1);
+		double dist = Double.MAX_VALUE;
+		int id = -1;
+		double tmp;
+		for (int point : points) {
+			tmp = getDistance(x, y, Data.x_dim[point], Data.x_dim[point]);
+			if (tmp < dist) {
+				dist = tmp;
+				id = point;
+			}
+		}
+		return id;
+	}
+
+	/**
+	 * test this it might have a off by one error
+	 * 
+	 * @param x
+	 * @param y
+	 * @param range
+	 * @return
+	 */
+	private static ArrayList<Integer> getPointsInBound(int x, int y, int range) {
+		if (range % 2 != 1) {
+			System.out.println("wrong range");
+			return new ArrayList<Integer>();
+		}
+		ArrayList<Integer> points = new ArrayList<Integer>();
+		ArrayList<Integer> points_x = new ArrayList<Integer>();
+		ArrayList<Integer> points_y = new ArrayList<Integer>();
+		while (points.isEmpty()) {
+			for (int i = 0; i < range; i++) {
+				for (int index: Data.gridX[x - range + i]) {
+					points_x.add(index);
+				}
+				for (int index: Data.gridY[x - range + i]) {
+					points_y.add(index);
+				}
+			}
+			points = calcIntersection(points_x, points_y);
+			range += 2;
+		}
+		// vielleicht muessen wir hier noch einmal alles aussenrum nehmen und mit
+		// berechnen, da es moeglcherweise doch naeher sein koennte
+		return points;
+	}
+
+	/**
+	 * ich kapier hier nicht wie du ohne for schleifen irgendwie alles abgreifen
+	 * willst. für mich sieht das nachm eindimensionalen einsammeln aus
+	 * 
+	 * @param x
+	 * @param y
+	 * @param r
+	 * @return
+	 */
+	public static int[][] getBounds(double x, double y, int r) {
+		int[] grid = getGridPosition(x, y); // ist das nicht dumm das hier zu machen? dann wird die methode ja bei jedem
+											// rekursiven aufruf aufgerufen? waers nicht besser der methode direkt nen
+											// array von getgridpos zu geben?
+		int[] x_bounds = { grid[0] - r, grid[0] + r };
+		int[] y_bounds = { grid[1] - r, grid[1] + r };
+		System.out.println(r);
+		if (getPointsInBounds(x_bounds[0], x_bounds[1], "x") == null
+				| getPointsInBounds(y_bounds[0], y_bounds[1], "y") == null) {
+			getBounds(x, y, r + 1);
+		}
+		return new int[][] { x_bounds, y_bounds };
+	}
+
+	/**
+	 * ja an sich ganz ok, allerdings die sache mit der dimension find ich nicht so
+	 * geil, auch wenn die Idee wahrscheinlich von mir kam.
+	 * 
+	 * @param low
+	 * @param up
+	 * @param dim
+	 * @return
+	 */
+	private static LinkedList<Integer> getPointsInBounds(int low, int up, String dim) {
+		LinkedList<Integer> pointsInBounds = new LinkedList<>();
+		switch (dim) {
+		case "x":
+			for (int i = low; i <= up; i++) {
+				pointsInBounds.addAll(Data.gridX[i]);
+			}
+			break;
+		case "y":
+			for (int i = low; i <= up; i++) {
+				pointsInBounds.addAll(Data.gridY[i]);
+			}
+			break;
+		default:
+			System.out.println("No proper dimension");
+		}
+
+		return pointsInBounds;
 	}
 }
